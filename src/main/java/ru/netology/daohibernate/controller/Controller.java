c0594bc723f02e52b5c30a802214f4dc577f0acf
Elena Ollivin <fortnokses@gmail.com>
2025-01-10 16:04:48 +0300

    create new file

--- src/main/java/ru/netology/daohibernate/controller/Controller.java
+++ src/main/java/ru/netology/daohibernate/controller/Controller.java
@@ -1,10 +1,13 @@
 package ru.netology.daohibernate.controller;
 
-import org.springframework.web.bind.annotation.*;
 import ru.netology.daohibernate.entity.Person;
 import ru.netology.daohibernate.service.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Controller {
@@ -17,5 +20,15 @@ public class Controller {
     @GetMapping("/persons/by-city")
     public List<Person> getPersons(@RequestParam("city") String city) {
         return service.getPersonsByCity(city);
+    }
+
+    @GetMapping("/persons/by-age")
+    public List<Person> getAge(@RequestParam("age") int age) {
+        return service.getAge(age);
+    }
+
+    @GetMapping("/persons/by-fullname")
+    public Optional<Person> getNameSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+        return service.getNameSurname(name, surname);
     }
 }
--- src/main/java/ru/netology/daohibernate/service/Service.java
+++ src/main/java/ru/netology/daohibernate/service/Service.java
@@ -5,6 +5,8 @@ import ru.netology.daohibernate.entity.Person;
 import ru.netology.daohibernate.repository.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @org.springframework.stereotype.Service
 public class Service {
@@ -15,6 +17,14 @@ public class Service {
     }
 
     public List<Person> getPersonsByCity(String city) {
-        return repository.getPersonsByCity(city);
+        return repository.findByCityOfLiving(city);
+    }
+
+    public List<Person> getAge(int age) {
+        return repository.findByAgeLessThanOrderByAge(age);
+    }
+
+    public Optional<Person> getNameSurname(String name, String surname) {
+        return repository.findByNameAndSurname(name, surname);
     }
 }
